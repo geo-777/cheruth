0f86d192af9c17273a9a5129b86e3fd4bc91a9d8
geo-777 <geo.binu.25@gmail.com>
2026-06-10 21:24:57 +0530

    updated backend files

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,17 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
-import { AuthService } from './providers/auth.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {
-  constructor(private authService: AuthService) {}
-
-  @Post('register') // POST /auth/register
-  register() {
-    return this.authService.register();
-  }
-
-  @Post('login') // POST /auth/login
-  login() {
-    return this.authService.login();
-  }
-}
+export class AuthController {}
